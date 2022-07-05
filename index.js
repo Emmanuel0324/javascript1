53216cfa7ca0815de1297b1ac0bcd142c8f0519b
Emmanuel0324 <105252086+Emmanuel0324@users.noreply.github.com>
2022-07-06 00:20:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var firstName = 'Emmanuel'
+var lastName = "Agana"
+var height = "6ft"
+var country = "Nigeria"
+
+name = firstName + ' ' + lastName;
+
+document.write('Name:' + '' + name + '<br/>')
+document.write('Height:' + '' + height + '<br>')
+document.write('Country:' + '' + country + '<br>')
+
+
